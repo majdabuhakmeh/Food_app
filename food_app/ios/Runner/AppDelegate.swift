cc62c5b51ce05d9895d70ff5b2f96ff396ff2703
majdabuhakmeh <majd.abuhakmeh@gmail.com>
2026-01-17 14:46:27 +0100

    Update Google Maps API key in AppDelegate

--- food_app/ios/Runner/AppDelegate.swift
+++ food_app/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-  GMSServices.provideAPIKey("AIzaSyA74JHPzQaTqBY_IhQnf_L4MwMfILYxfPM")
+  GMSServices.provideAPIKey("00000000000")
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
